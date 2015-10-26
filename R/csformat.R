6b4638ffd2430b4b510a2099a1a5b112cb3abb0e
Laurent Dubroca <laurent.dubroca@gmail.com>
2015-10-26 14:07:12 +0100

    cs excel file added

---
+++ R/csformat.R
@@ -0,0 +1,9 @@
+#' CS format definition
+#'
+#' An excel file defining the csPi format
+#' included in the data directory of the fishPifct package
+#'
+#' @format xlsx file
+#' @source WKRDB sharepoint \url{http://www.ices.dk}
+#' @name csformat.xlsx
+NULL
