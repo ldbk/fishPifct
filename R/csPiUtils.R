9ad081d18cd5c8074fef20edc7b9f30b75fda3ff
Laurent Dubroca <laurent.dubroca@gmail.com>
2016-03-03 23:08:58 +0100

    export function improved

--- R/csPiUtils.R
+++ R/csPiUtils.R
@@ -124,9 +124,142 @@ setMethod("dim", signature("csPi"), function(x){
 #====================================================================
 # 'export' function
 #====================================================================
-#' Export csPi or csData object in excel.
+#' Export csPi or csData object in excel, csv or SQLite files.
 #'
 #' @param object: a csPi or csData object.
+#' @param file: a file name.
+#' @param type: "csv" or "xlsx" or "SQLite"
+#'
+#' @return file path of the generated file(s).
+#'
+#' @seealso \code{link{exportxlsx}}, \code{link{exportcsv}} and \code{link{exportdb}} 
+#'
+#' @examples
+#'\dontrun{
+#'  data(sole)
+#'  #xlsx export
+#'  export(sole.cs,file="sole.xlsx")
+#'  #csv export
+#'  export(sole.cs,file="sole.csv")
+#'  #SQLite export
+#'  export(sole.cs,file="sole.sqlite3")
+#' }
+#'
+#' @author Norrent Bibroca & Laurbert Dullet 
+#'
+#' @export
+setGeneric("export", function(object, file, type,...){
+		   	standardGeneric("export")
+				}
+)
+#' @export
+setMethod("export", signature("csPi","csData"), function(object,file="output.xlsx",type="xlsx",...){
+ if(!(type%in%c("csv","xlsx","SQLite"))){type<-"notype"}
+ switch(type,
+	csv={filename<-exportcsv(object,file)
+	},
+        xlsx={filename<-exportxslx(object,file)	
+	},
+	SQLite={filename<-exportdb(object,file)
+ 	},
+	notype={print("wrong output type, please use 'xlsx', 'csv' or 'SQLite'")
+	}
+	)
+  	return(filename)
+}
+)
+#====================================================================
+# 'exportxlsx' function
+#====================================================================
+#' Export a S4 object in an excel file.
+#'
+#' @param object: a S4 object
+#' @param file: a file name.
+#'
+#' @return file path of the generated file(s).
+#'
+#' @examples
+#'\dontrun{
+#'  data(sole)
+#'  exportxlsx(sole.cs,file="sole.xlsx")
+#' }
+#'
+#' @author Norrent Bibroca & Laurbert Dullet 
+#'
+#' @export
+exportxlsx<-function(object,filename){
+	objslot<-slotNames(object)
+	listmainslot<-
+	if(is.null(objslot)){
+		print("Object has no slot, check your object. Nothing to export.")
+	}else{
+		#generate wb
+  		wb <- createWorkbook()
+  		## Add worksheets and data
+		for(i in 1:length(objslot)){
+			addWorksheet(wb,objslot[i])
+			slottmp<-slot(object,objslot[i])
+			if(class(slottmp)=="character"){slottmp<-data.frame(slottmp);names(slottmp)<-as.character(objslot[i])}
+  			#eval(parse(text=paste0("writeData(wb,'",objslot[i],"',object@",objslot[i],")")))
+  			writeData(wb,objslot[i],slottmp)
+		}
+		#saving the workbook
+  		saveWorkbook(wb, filename, overwrite = TRUE)
+	}
+	return(filename)
+}
+#====================================================================
+# 'exportcsv' function
+#====================================================================
+#' Export a S4 object in multiple csv files.
+#' The csv files are generated using the name provided and adding the object slotname in the name between the name
+#' and the file extension (usually '.csv'). If the file name provided has no extension, '.csv' is added to the name.
+#'
+#' @param object: a S4 object
+#' @param file: a base for the file names.
+#'
+#' @return files path of the generated files.
+#'
+#' @examples
+#'\dontrun{
+#'  data(sole)
+#'  exportcsv(sole.cs,file="sole.csv")
+#' }
+#'
+#' @author Naurrent Bibrolet & Lorbert Dullca 
+#'
+#' @export
+exportcsv<-function(object,filename){
+	objslot<-slotNames(object)
+	listfilename<-data.frame()
+	if(is.null(objslot)){
+		print("Object has no slot, check your object. Nothing to export.")
+	}else{
+		#generate wb
+		for(i in 1:length(objslot)){
+			dotpos<-max(gregexpr("\\.",filename)[[1]])
+			if(dotpos!=-1){
+				filenameslot<-paste0(substr(filename,1,dotpos-1),"_",objslot[i],substr(filename,dotpos,nchar(filename)))
+			}else{
+				filenameslot<-paste0(filename,"_",objslot[i],".csv")
+			}
+			listfilename<-rbind(listfilename,filenameslot)
+			slottmp<-slot(object,objslot[i])
+			if(class(slottmp)=="character"){slottmp<-data.frame(slottmp);names(slottmp)<-as.character(objslot[i])}
+			con<-file(filenameslot,"at")
+			write.csv(slottmp,con,row.names=F)
+			close(con)
+		}
+	}
+	names(listfilename)<-"filename"
+	return(listfilename)
+}
+#====================================================================
+# 'exportdb' function
+#====================================================================
+#' Export an S4 object to a SQLite database.
+#'
+#' @param object: an S4 object
 #' @param file: a file name.
 #'
 #' @return file path of the generated file.
@@ -134,41 +267,53 @@ setMethod("dim", signature("csPi"), function(x){
 #' @examples
 #'\dontrun{
 #'  data(sole)
-#'  #csData export
-#'  export(sole.cs,file="output1.xlsx")
-#'  #csPi export
-#'  export(pipo,file="output2.xlsx")
+#'  exportdb(sole.cs,file="sole.sqlite3")
 #' }
+#'
+#' @author Nobent Dibroca & Laurrert Bullet 
+#'
 #' @export
-#' @author Laurent Dubroca & Norbert Billet
-setGeneric("export", function(object, file, ...){
-		   	standardGeneric("export")
-				}
-)
-#csPi format
-#' @export
-setMethod("export", signature("csPi"), function(object,file="csPi.xlsx",...){
-  wb <- createWorkbook()
-  ## Add worksheets
-  addWorksheet(wb, "info")
-  addWorksheet(wb, "se")
-  addWorksheet(wb, "tr")
-  addWorksheet(wb, "hh")
-  addWorksheet(wb, "sl")
-  addWorksheet(wb, "hl")
-  addWorksheet(wb, "ca")
-  info<-data.frame(classVersion=object@classVersion,desc=object@desc,popData=object@popData,design=object@design,date=date())
-  writeData(wb, "info", t(info), rowNames = TRUE,colNames=FALSE)
-  writeData(wb, "se", object@se, startCol = 1, startRow = 1, rowNames = FALSE)
-  writeData(wb, "tr", object@tr, startCol = 1, startRow = 1, rowNames = FALSE)
-  writeData(wb, "hh", object@hh, startCol = 1, startRow = 1, rowNames = FALSE)
-  writeData(wb, "sl", object@sl, startCol = 1, startRow = 1, rowNames = FALSE)
-  writeData(wb, "hl", object@hl, startCol = 1, startRow = 1, rowNames = FALSE)
-  writeData(wb, "ca", object@ca, startCol = 1, startRow = 1, rowNames = FALSE)
-  saveWorkbook(wb, file, overwrite = TRUE)
-  return(file)
+exportdb<- function(object,file) {
+	#data(sole)
+	#pipo <- csDataTocsPi(sole.cs)
+	#csObj<-pipo
+	if(!require(RSQLite)){
+		   print("RSQLite package needed")
+	}else{
+	require(RSQLite)
+	#file<-"sqlite.db"
+	# Create a database in memory.
+	con <- dbConnect(SQLite(),dbname=file)#, ":memory:")
+	#select slot
+	slotlist<-slotNames(object)
+	for(idslot in slotlist){
+		slottmp<-slot(object,idslot)
+		if(class(slottmp)=="character"){slottmp<-data.frame(slottmp);names(slottmp)<-as.character(idslot)}
+		dbWriteTable(con,idslot,slottmp,overwrite=TRUE)
+	}
+	dbDisconnect(con)
+	print(paste0("SQLite db created in ",file))
+	return(file)
+	}
 }
-)
+
+#test area
+#library(fishPifct)
+#data(sole.cs)
+### Not run: 
+#data(sole)
+#pipo <- csDataTocsPi(sole.cs)
+#head(pipo)
+#object<-pipo
+#filename<-"test.csv"
+#exportxlsx(sole.cs,"test.xlsx")
+#exportxlsx(pipo,"test.xlsx")
+#exportcsv(pipo,"test2")
+#exportcsv(pipo,"te.st2.adada")
+#exportdb(sole.cs,"pipo.sqlite3")
+
+
+
 #COSTcore format
 setMethod("export", signature("csData"), function(object,file="csData.xlsx",...){
   wb <- createWorkbook()
